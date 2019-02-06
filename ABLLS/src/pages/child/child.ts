8f81cb4ea6e8592361d3c1baf676ec012b34ffbf
Hemanth Gundrajukuppam <hemgundr@publicisgroupe.net>
2019-02-06 16:14:38 +0530

    Typecasting element

--- ABLLS/src/pages/child/child.ts
+++ ABLLS/src/pages/child/child.ts
@@ -204,7 +204,7 @@ export class ChildPage implements OnInit {
 
   pdf(){
     const data = document.getElementById('contentToConvert');
-    const elements = data.getElementsByClassName('editIcon');
+    const elements = data.getElementsByClassName('editIcon') as HTMLCollectionOf<HTMLElement> || [];
     for(let i=0; i<elements.length; i++) {
       elements[i].style.display = 'none';
     }
