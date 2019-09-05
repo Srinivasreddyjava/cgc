3cbf6798ce6229fc2178e253d11a2bcd411e9755
slyagala <slyagala@cisco.com>
2019-09-05 21:01:41 +0530

    employee image update

--- ABLLS/src/pages/employee/employee.ts
+++ ABLLS/src/pages/employee/employee.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { IonicPage, NavController, NavParams, LoadingController, ToastController, AlertController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController, ToastController, AlertController,ActionSheetController, Platform } from 'ionic-angular';
 import { AuthService } from '../../services/auth';
 import { AssignEmployeePage } from '../assign-employee/assign-employee';
 import { ChildPage } from '../child/child';
+import { Camera } from '@ionic-native/camera';
 
 @IonicPage()
 @Component({
@@ -11,7 +12,16 @@ import { ChildPage } from '../child/child';
 })
 export class EmployeePage implements OnInit{
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private auth: AuthService, private loadCtrl: LoadingController, private toast: ToastController, private alertCtrl: AlertController) {}
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
+    private auth: AuthService,
+    private loadCtrl: LoadingController,
+    private toast: ToastController,
+    private alertCtrl: AlertController,
+    public actionSheetCtrl: ActionSheetController,
+    public platform: Platform,
+    private camera: Camera
+  ) {}
 
   emp;
   name;
@@ -27,13 +37,17 @@ export class EmployeePage implements OnInit{
   }
 
   ngOnInit(){
-    this.emp = this.navParams.get('employee');  
+    this.emp = this.navParams.get('employee');
     this.name= this.emp.name;
     this.email= this.emp.email;
     this.mobile= this.emp.mobile;
-    this.password= this.emp.password;  
-    this.number = this.emp.number;
-
+    this.password= this.emp.password;
+    this.auth.getStaffImage(this.emp._id).subscribe(res=>{
+      if(res.success){
+        this.empImage=res.msg;
+      }else{
+      }
+    })
     this.getEmpChildren();
   }
   updateEmployee(){
@@ -46,8 +60,7 @@ export class EmployeePage implements OnInit{
       name: this.name,
       email: this.email,
       mobile: this.mobile,
-      password: this.password,
-      number: this.number
+      password: this.password
     }
     this.auth.updateEmployee(emp).subscribe(res => {
       if(res.success){
@@ -74,6 +87,13 @@ export class EmployeePage implements OnInit{
   getEmpChildren(){
     this.auth.getEmpChildren(this.emp._id).subscribe(res => {
       if(res.success) {
+        if(res.msg.length>0){
+          res.msg.forEach(child =>{
+            var staff=child.staff;
+            console.log(JSON.parse(staff))
+            child.staff=JSON.parse(staff);
+          })
+        }
         this.emp_children = res.msg;
       }else {
         // Something went wrong
@@ -85,7 +105,7 @@ export class EmployeePage implements OnInit{
   }
 
   deregisterEmployee() {
-    
+
     const alert = this.alertCtrl.create({
       message: 'This action is not reversible',
       buttons: [
@@ -117,6 +137,117 @@ export class EmployeePage implements OnInit{
       ]
     });
     alert.present();
+  }
+  presentAccessControl(){
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'Select Image Source',
+      buttons: [
+        {
+          text: 'Load from Library',
+          handler: () => {
+            this.takePicture(this.camera.PictureSourceType.PHOTOLIBRARY);
+          }
+        },
+        {
+          text: 'Use Camera',
+          handler: () => {
+            this.takePicture(this.camera.PictureSourceType.CAMERA);
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        }
+      ]
+    });
+    actionSheet.present();
+  }
+
+  takePicture(sourceType) {
+  // Create options for the Camera Dialog
+  var options = {
+    quality: 25,
+    destinationType:this.camera.DestinationType.DATA_URL,
+    encodingType: this.camera.EncodingType.JPEG,
+    sourceType: sourceType,
+    saveToPhotoAlbum: false,
+    correctOrientation: true,
+    allowEdit:true
+  };
+
+  // Get the data of an image
+  this.camera.getPicture(options).then((imagePath) => {
+    // Special handling for Android library
+    console.log("imagePath:"+imagePath);
+    this.empImage="data:image/jpg;base64,"
+    if (this.platform.is('android') && sourceType === this.camera.PictureSourceType.PHOTOLIBRARY) {
+      this.empImage+=imagePath;
+      this.uploadImage()
+    } else {
+      this.empImage+=imagePath;
+      this.uploadImage()
+    }
+  }, (err) => {
+    this.presentToast('Error while selecting image.');
+  });
+}
+
+uploadImage(){
+  this.generateFromImage(this.empImage,200,200,0.5,data=>{
+    this.empImage=data;
+    var obj={
+      image:this.empImage
+    }
+    this.auth.uploadStaffImage(this.emp._id,obj).subscribe(res =>{
+      if(res.success){
+        this.presentToast("Image Updated SuccessFully")
+      }else{
+        this.presentToast(res.msg)
+      }
+    })
+  })
+}
+
+presentToast(text) {
+  let toast = this.toast.create({
+    message: text,
+    duration: 3000,
+    position: 'top'
+  });
+  toast.present();
+}
+
+generateFromImage(img, MAX_WIDTH: number = 700, MAX_HEIGHT: number = 700, quality: number = 1, callback) {
+    var canvas: any = document.createElement("canvas");
+    var image = new Image();
+
+    image.onload = () => {
+      var width = image.width;
+      var height = image.height;
+
+      if (width > height) {
+        if (width > MAX_WIDTH) {
+          height *= MAX_WIDTH / width;
+          width = MAX_WIDTH;
+        }
+      } else {
+        if (height > MAX_HEIGHT) {
+          width *= MAX_HEIGHT / height;
+          height = MAX_HEIGHT;
+        }
+      }
+      canvas.width = width;
+      canvas.height = height;
+      var ctx = canvas.getContext("2d");
+
+      ctx.drawImage(image, 0, 0, width, height);
+
+      // IMPORTANT: 'jpeg' NOT 'jpg'
+      var dataUrl = canvas.toDataURL('image/jpeg', quality);
+
+      callback(dataUrl)
+    }
+    image.src = img;
   }
 
 }
